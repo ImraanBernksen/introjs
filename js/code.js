b43bf29e18b7d75873f37e184a0d474bdef20b06
ImraanBernksen <imraanbernksen@gmail.com>
2022-11-07 16:24:25 +0200

    done

---
+++ js/code.js
@@ -0,0 +1,23 @@
+// function madeBy() {
+//     alert("Made By Imraan Bernksen")
+
+// }
+
+// let madeBy = document.querySelector
+// ("button");
+
+// Add listener
+// madeBy.addEventListener('click', ()=>{
+//     alert("Made By Imraan Bernksen")
+// });
+
+// Write to a document
+// document.write("Hello World");
+// document.write("<br>My name is Lionel Messi");
+// document.write("<br>Am a Soccer Player");
+
+let userName = 'Imraan'
+let userAge = '20'
+console.log(userName);
+console.log(userAge);
+console.log(typeof userName);
