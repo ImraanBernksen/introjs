e5dbfb5d01e8bf0b026ff218eb32ec5a03220c69
ImraanBernksen <imraanbernksen@gmail.com>
2022-11-08 10:00:46 +0200

    awe

--- js/code.js
+++ js/code.js
@@ -16,8 +16,34 @@
 // document.write("<br>My name is Lionel Messi");
 // document.write("<br>Am a Soccer Player");
 
-let userName = 'Imraan'
-let userAge = '20'
-console.log(userName);
-console.log(userAge);
-console.log(typeof userName);
+// let userName = 'Imraan'
+// let userAge = '20'
+// console.log(userName);
+// console.log(userAge);
+// console.log(typeof userName);
+
+// Revision
+// numb1 = 8;
+// let userName = 'Imraan';
+// const vat = .16;
+// var surname = 'Bernksen';
+// var data;
+// console.log(surname);
+// let numb1 =10+9+'Imraan'+9+11;
+// console.log(numb1);
+
+// let numb1 = prompt("Please enter numb1: ");
+// // 5
+// let numb2 = prompt("Please enter numb2: ");
+// let addition = numb1 + numb2;
+// console.log(addition);
+
+// let numb1 = 4;
+// let numb2 = 3;
+// let product = eval(`${numb1}*${numb2}`);
+// console.log(product);
+
+let firstName = 'Lionel';
+let age = 35;
+// console.log("My name is Imraan and am 20 year old");
+console.log("My name is ${firstname} and am ${age} year old");
